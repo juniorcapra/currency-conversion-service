ea1e58296f056e78356207c974f589ef0038cbb8
juniorcapra <acaprajr@gmail.com>
2020-08-27 21:04:37 -0300

    Client Feign

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,9 @@ package com.in28minutes.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,11 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
 
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
@@ -30,8 +38,19 @@ public class CurrencyConversionController {
         return new CurrencyConversionBean(response.getId(), from, to,
             response.getConversionMultiple(), quantity,
             quantity.multiply(response.getConversionMultiple()), response.getPort());
-
-
     }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+        @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+        logger.info("{}", response);
+
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+            quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+
 
 }
